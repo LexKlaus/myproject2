265feae183976160a095d5871f616b77a7717a8b
LexKlaus <luthor47@gmail.com>
2019-07-28 19:24:56 +0800

    Java and Kotlin variable,data type

--- src/main/java/com/Lex/Hello.java
+++ src/main/java/com/Lex/Hello.java
@@ -6,5 +6,15 @@ public class Hello {
         //new Person().hello();
         Person p = new Person();
         p.hello();
+        int age = 19;
+        char c ='我';
+        byte b = 120;
+        float weight = 66.5f;
+        float height = 1.7f;
+        boolean adult = true;
+        boolean enrol = false;
+        String name = "Tom";
+        
+
     }
 }
